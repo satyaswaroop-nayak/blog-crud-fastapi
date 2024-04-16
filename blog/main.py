525944bdf5d8b554bf70a0f71de9a8e9d4453a18
Satyaswaroop Nayak <satyanayakp001@gmail.com>
2024-04-17 02:25:13 +0530

    response model commit

--- blog/main.py
+++ blog/main.py
@@ -25,11 +25,12 @@ def getBlogs(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def getBlog(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).all()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found")
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
     return blog
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
@@ -40,12 +41,17 @@ def updateBlog(id: int, request:schemas.Blog, db:Session=Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Blog with id {id} not found")
 
-    blog.update(request)
+    blog.update(dict(request))
     db.commit()
     return 'updated'
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def deleteBlog(id, db:Session=Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+
+    blog.delete(synchronize_session=False)
     db.commit()
     return {"response":"deleted"}
--- blog/schemas.py
+++ blog/schemas.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+class ShowBlog(Blog):
+    class Config():
+        orm_mode = True
